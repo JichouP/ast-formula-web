c7dc69b2eba9f512cb4fcaea348451f575e2a258
JichouP <akkunn100@gmail.com>
2022-07-05 23:14:51 +0900

    :sparkles: add App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import init, { calculate } from '../wasm/pkg';
 
 function App() {
+  const [value, setValue] = useState('1+2');
+  const [answer, setAnswer] = useState('aa');
+  const [initDone, setInitDone] = useState(false);
+
   useEffect(() => {
-    init();
+    init().then(() => setInitDone(true));
   }, []);
-  return <div></div>;
+
+  useEffect(() => {
+    if (initDone) {
+      try {
+        const result = `${calculate(value)}`;
+        setAnswer(result);
+      } catch (e) {
+        setAnswer(e as string);
+      }
+    }
+  }, [value]);
+
+  return (
+    <div className="flex h-screen justify-center">
+      <div className="flex flex-col items-center">
+        <div className="m-16 text-center text-4xl">WASM Calculator</div>
+        <input
+          type="text"
+          placeholder="Type expression here"
+          className="input input-bordered m-8 w-80 text-2xl"
+          value={value}
+          onChange={(e) => {
+            setValue(e.currentTarget.value);
+          }}
+        ></input>
+        <div>
+          <span className="text-2xl">{answer}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
